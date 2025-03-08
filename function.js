1d3e846234524cef7f242e0c40e0c4629fccb282
MaMcSoft <mamcsoft92@gmail.com>
2025-03-08 16:09:28 -0600

    multi sheet support

--- function.js
+++ function.js
@@ -1,19 +1,29 @@
-window.function = async function(url, cells) {
+window.function = async function(url, cells, index) {
   try {
-    const fileUrl = url.value ?? "";
-    const cellsRange = cells.value ?? "";
+    const fileUrl = url.value.split(",") ?? "";
+    const cellsRange = cells.value.split(",") ?? "";
+    const sheetsIndex = index.value.split(",") ?? "";
     const response = await fetch(fileUrl);
+
+    let results = [];
+
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    const arrayBuffer = await response.arrayBuffer();
-    const data = new Uint8Array(arrayBuffer);
-    const workbook = XLSX.read(data, { type: "array" });
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const jsonData = XLSX.utils.sheet_to_json(worksheet, { range: cellsRange });
-    const extract = JSON.stringify(jsonData, null, 2);
-    console.log("Extracted Data:", extract);
-    return extract;
+
+    for (let i = 0; i < sheetsIndex.length; i++) {
+      let sheet = sheetsIndex[i];
+      const arrayBuffer = await response.arrayBuffer();
+      const data = new Uint8Array(arrayBuffer);
+      const workbook = XLSX.read(data, { type: "array" });
+      const worksheet = workbook.Sheets[workbook.SheetNames[sheet]];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { range: cellsRange[i] });
+      //const extract = JSON.stringify(jsonData, null, 2);
+      console.log("Extracted Data:", extract);
+      results.push(jsonData);
+  }
+    return JSON.stringify(results, null, 2);
+
   } catch (error) {
     console.error("Error fetching or processing the Excel file:", error);
   }
