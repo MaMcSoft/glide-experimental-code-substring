8ed28966e4e9eb57dfddaaf4d867cbe9a290dbf0
MaMcSoft <mamcsoft92@gmail.com>
2025-03-08 12:13:24 -0600

    v1.0

--- function.js
+++ function.js
@@ -18,38 +18,3 @@ window.function = async function(url, cells) {
     console.error("Error fetching or processing the Excel file:", error);
   }
 }
-
-/*window.function = function (url, cells) {
-
-  fileUrl = url.value ?? "";
-  cellsRange = cells.value ?? "";
-  var extract = "";
-
-  fetch(fileUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.arrayBuffer();
-    })
-    .then(arrayBuffer => {
-      // Convert the ArrayBuffer to a Uint8Array
-      const data = new Uint8Array(arrayBuffer);
-      // Read the workbook from the binary data
-      const workbook = XLSX.read(data, { type: 'array' });
-      // Select the first worksheet (adjust if needed)
-      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-      // Define the range you want to extract (for example, A1:D10)
-      const range = cellsRange;
-      // Convert the specified range to JSON.
-      // By default, the first row is used as the header row.
-      const jsonData = XLSX.utils.sheet_to_json(worksheet, { range: range });
-      
-      extract = JSON.stringify(jsonData, null, 2)
-      return extract;
-    })
-    .catch(error => {
-      console.error('Error fetching or processing the Excel file:', error);
-    });
-    
-}*/
